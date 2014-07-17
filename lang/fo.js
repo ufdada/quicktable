e50161aed971c550c27b0fa7ea8b3798b251315a
ufdada <bloody_impact@web.de>
2014-07-17 12:23:45 +0200

    Revert "defining global CKEDITOR"
    This reverts commit 4b677139d7f72d52f30d212d72fc4d075e88ffbc.

--- lang/fo.js
+++ lang/fo.js
@@ -1,10 +1,7 @@
 ﻿/*
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or http:///*global CKEDITOR*/
-CKEDITOR.com/license
+For licensing, see LICENSE.md or http://ckeditor.com/license
 */
-/*global CKEDITOR*/
-/*global CKEDITOR */
 CKEDITOR.plugins.setLang( 'quicktable', 'fo', {
 	more: 'Fleiri...'
 } );
