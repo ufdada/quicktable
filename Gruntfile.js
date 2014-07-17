a0adb83b804a4352649fe43c22a4ac9c2ed017e7
ufdada <bloody_impact@web.de>
2014-07-17 10:40:27 +0200

    adding hint for linting

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+/* jshint node: true */
+
+module.exports = function (grunt) {
+	"use strict";
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'), 
+		jshint: {
+			all: [
+				"lang/*.js", 
+				"*.js"
+			], 
+			options: {
+				jshintrc: '.jshintrc'
+			},
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint')
+
+	grunt.registerTask('test', ['jshint'])
+	grunt.registerTask('default', ['test'])
+};
