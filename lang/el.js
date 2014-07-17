4b677139d7f72d52f30d212d72fc4d075e88ffbc
ufdada <bloody_impact@web.de>
2014-07-17 12:21:09 +0200

    defining global CKEDITOR

--- lang/el.js
+++ lang/el.js
@@ -1,7 +1,10 @@
 ﻿/*
 Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.md or http://ckeditor.com/license
+For licensing, see LICENSE.md or http:///*global CKEDITOR*/
+CKEDITOR.com/license
 */
+/*global CKEDITOR*/
+/*global CKEDITOR */
 CKEDITOR.plugins.setLang( 'quicktable', 'el', {
 	more: 'Περισσότερα...'
 } );
